b4af1c5fdc543611e7d355b83461d75249344daf
machamp0714 <rateliasudaisuki@gmail.com>
2020-06-06 11:36:54 +0900

    first commit

---
+++ lib/pokemon_translator.rb
@@ -0,0 +1,6 @@
+require "pokemon_translator/version"
+
+module PokemonTranslator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pokemon_translator/version.rb
@@ -0,0 +1,3 @@
+module PokemonTranslator
+  VERSION = "0.1.0"
+end
